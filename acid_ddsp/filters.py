0fbbdb24ffaaaec48b626ff22123a7268d939689
christhetree <christhetree@gmail.com>
2024-02-18 14:42:48 +0000

    [cm] Cutoff working but not resonance

--- acid_ddsp/filters.py
+++ acid_ddsp/filters.py
@@ -21,9 +21,9 @@ def time_varying_fir(x: T, b: T) -> T:
     assert b.ndim == 3
     assert x.size(0) == b.size(0)
     assert x.size(1) == b.size(1)
-    order = b.size(2)
-    x_padded = F.pad(x, (order - 1, 0))
-    x_unfolded = x_padded.unfold(dimension=1, size=order, step=1)
+    order = b.size(2) - 1
+    x_padded = F.pad(x, (order, 0))
+    x_unfolded = x_padded.unfold(dimension=1, size=order + 1, step=1)
     x_unfolded = x_unfolded.unsqueeze(3)
     b = b.unsqueeze(2)
     y = b @ x_unfolded
@@ -42,12 +42,12 @@ class TimeVaryingBiquad(nn.Module):
         stability_eps: float = 1e-3,
     ):
         super().__init__()
-        assert 0.0 < min_w
-        assert 0.0 < max_w <= 2 * tr.pi
-        self.min_w = tr.tensor(min_w)
-        self.max_w = tr.tensor(max_w)
-        self.min_q = tr.tensor(min_q)
-        self.max_q = tr.tensor(max_q)
+        assert 0.0 <= min_w <= max_w <= tr.pi
+        assert 0.0 < min_q <= max_q
+        self.min_w = tr.tensor(min_w).clamp(stability_eps, tr.pi - stability_eps)
+        self.max_w = tr.tensor(max_w).clamp(stability_eps, tr.pi - stability_eps)
+        self.min_q = tr.tensor(min_q).clamp_min(stability_eps)
+        self.max_q = tr.tensor(max_q).clamp_min(stability_eps)
         self.log_min_w = tr.log(self.min_w)
         self.log_max_w = tr.log(self.max_w)
         self.log_min_q = tr.log(self.min_q)
@@ -59,6 +59,8 @@ class TimeVaryingBiquad(nn.Module):
         w = tr.exp(log_w)
         log_q = self.log_min_q + (self.log_max_q - self.log_min_q) * mod_sig_q
         q = tr.exp(log_q)
+        # q_db = 20 * tr.log10(q)
+        # q = q_db
 
         alpha_q = tr.sin(w) / (2 * q)
 
@@ -69,17 +71,24 @@ class TimeVaryingBiquad(nn.Module):
         a2 = 1.0 - alpha_q
         a2 /= a0
         # a2 = (1.0 - self.stability_eps) * a2
-        a0.fill_(1.0)
         assert (a1.abs() < 2.0).all()
         assert (a2 < 1.0).all()
         assert (a1 < a2 + 1.0).all()
         assert (a1 > -(a2 + 1.0)).all()
-
-        a = tr.stack([a0, a1, a2], dim=2)
+        # log.info(
+        #     f"a0[0,0] = {a0[0, 0]:.4f}, a1[0, 0] = {a1[0, 0]:.4f}, a2[0, 0] = {a2[0, 0]:.4f}"
+        # )
+        a = tr.stack([a1, a2], dim=2)
 
         b0 = (1.0 - tr.cos(w)) / 2.0
+        b0 /= a0
         b1 = 1.0 - tr.cos(w)
+        b1 /= a0
         b2 = (1.0 - tr.cos(w)) / 2.0
+        b2 /= a0
+        # log.info(
+        #     f"b0[0,0] = {b0[0, 0]:.4f}, b1[0, 0] = {b1[0, 0]:.4f}, b2[0, 0] = {b2[0, 0]:.4f}"
+        # )
         b = tr.stack([b0, b1, b2], dim=2)
 
         return a, b
@@ -106,13 +115,10 @@ class TimeVaryingBiquad(nn.Module):
         assert resonance_mod_sig.max() <= 1.0
         a_coeffs, b_coeffs = self._calc_coeffs(cutoff_mod_sig, resonance_mod_sig)
         y_a = sample_wise_lpc(x, a_coeffs)
-        n_inf = tr.isinf(y_a).sum()
-        log.info(f"n_inf: {n_inf}")
         assert not tr.isinf(y_a).any()
         assert not tr.isnan(y_a).any()
-        # y_ab = time_varying_fir(x, b_coeffs)
-        # return y_ab
-        return y_a
+        y_ab = time_varying_fir(x, b_coeffs)
+        return y_ab
 
 
 if __name__ == "__main__":
@@ -144,30 +150,33 @@ if __name__ == "__main__":
     #     )
 
     tr.manual_seed(42)
-    sr = 4000
+    sr = 16000
     bs = 1
     min_f = 1000.0
-    max_f = 1000.0
-    min_q = 0.7071
-    max_q = 0.7071
+    max_f = 4000.0
+    min_q = 4.7071
+    max_q = 4.7071
 
     min_w = 2 * tr.pi * min_f / sr
     max_w = 2 * tr.pi * max_f / sr
     tvb = TimeVaryingBiquad(min_w, max_w, min_q, max_q)
-    lfo = tr.linspace(0.0, 1.0, sr).unsqueeze(0).repeat(bs, 1).double()
 
-    n_samples = sr
-    white_noise = tr.randn((bs, n_samples)).double()
+    n_samples = 2 * sr
+    white_noise = tr.rand((bs, n_samples))
+    lfo = tr.linspace(0.0, 1.0, n_samples).unsqueeze(0).repeat(bs, 1)
+    # lfo = tr.linspace(1.0, 0.0, n_samples).unsqueeze(0).repeat(bs, 1)
+    # lfo = None
 
     x = white_noise
     log.info("start")
-    y = tvb(x, cutoff_mod_sig=lfo)
+    y = tvb(x, cutoff_mod_sig=lfo, resonance_mod_sig=lfo)
     log.info(f"y.shape: {y.shape}")
+    log.info(f"y[0, :4] = {y[0, :4]}")
+    torchaudio.save("../out/tmp.wav", y, sr)
 
     spec_transform = torchaudio.transforms.Spectrogram(n_fft=2048, hop_length=512)
     spec = spec_transform(y)
     log_spec = tr.log10(spec[0] + 1e-9)
-
     plt.imshow(
         log_spec,
         aspect="auto",
@@ -179,8 +188,7 @@ if __name__ == "__main__":
     # mag_response = tr.fft.rfft(y[0]).abs()
     # mag_response = mag_response / mag_response.max()
     # mag_response_db = 20 * tr.log10(mag_response)
-    #
-    # plt.plot(mag_response_db.detach().numpy())
+    # plt.plot(mag_response_db.numpy())
     # plt.show()
 
     # b_coeff_raw = [
