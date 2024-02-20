00f66210adad77179c4991a1a22f15f2884f4347
christhetree <christhetree@gmail.com>
2024-02-20 17:54:19 +0000

    [cm] Adding MidiF0ModSigDataset

---
+++ acid_ddsp/datasets.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+import torch as tr
+from torch import Tensor as T
+from torch.utils.data import Dataset
+
+from acid_ddsp.modulations import ModSignalGenerator
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
+
+
+class MidiF0ModSignalDataset(Dataset):
+    def __init__(self,
+                 min_midi_f0: int,
+                 max_midi_f0: int,
+                 mod_sig_gen: ModSignalGenerator,
+                 n_frames: int,
+                 num_examples_per_epoch: int):
+        super().__init__()
+        self.min_midi_f0 = min_midi_f0
+        self.max_midi_f0 = max_midi_f0
+        self.mod_sig_gen = mod_sig_gen
+        self.n_frames = n_frames
+        self.num_examples_per_epoch = num_examples_per_epoch
+
+    def __len__(self) -> int:
+        return self.num_examples_per_epoch
+
+    def __getitem__(self, idx: int) -> (T, T):
+        midi_f0 = tr.randint(self.min_midi_f0, self.max_midi_f0 + 1, (1,))
+        mod_sig = self.mod_sig_gen(self.n_frames)
+        return midi_f0, mod_sig
--- acid_ddsp/modulations.py
+++ acid_ddsp/modulations.py
@@ -13,7 +13,6 @@ log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 class ModSignalGenerator(nn.Module):
     def __init__(
         self,
-        n_frames: int,
         min_start_val: float = 0.0,
         max_start_val: float = 1.0,
         min_corner_val: float = 0.0,
@@ -26,12 +25,10 @@ class ModSignalGenerator(nn.Module):
         max_alpha: float = 6.0,
     ):
         super().__init__()
-        assert n_frames > 2
         assert 0.0 <= min_start_val <= max_start_val <= 1.0
         assert 0.0 <= min_corner_val <= max_corner_val <= 1.0
         assert 0.0 <= min_end_val <= max_end_val <= 1.0
         assert 0.0 <= min_attack_frac <= max_attack_frac <= 1.0
-        self.n_frames = n_frames
         self.min_start_val = min_start_val
         self.max_start_val = max_start_val
         self.min_corner_val = min_corner_val
@@ -43,7 +40,8 @@ class ModSignalGenerator(nn.Module):
         self.min_alpha = min_alpha
         self.max_alpha = max_alpha
 
-    def forward(self) -> T:
+    def forward(self, n_frames: int) -> T:
+        assert n_frames > 2
         start_val = (
             tr.rand() * (self.max_start_val - self.min_start_val) + self.min_start_val
         )
@@ -56,14 +54,14 @@ class ModSignalGenerator(nn.Module):
             tr.rand() * (self.max_attack_frac - self.min_attack_frac)
             + self.min_attack_frac
         )
-        corner_idx = (self.n_frames * corner_frac).int().clamp(1, self.n_frames - 2)
+        corner_idx = (n_frames * corner_frac).int().clamp(1, n_frames - 2)
 
-        mod_sig = tr.zeros(self.n_frames)
+        mod_sig = tr.zeros(n_frames)
         mod_sig[:corner_idx] = tr.linspace(
             start_val.item(), corner_val.item(), corner_idx.item()
         )
         mod_sig[corner_idx:] = tr.linspace(
-            corner_val.item(), end_val.item(), self.n_frames - corner_idx.item()
+            corner_val.item(), end_val.item(), n_frames - corner_idx.item()
         )
 
         alpha = tr.rand() * (self.max_alpha - self.min_alpha) + self.min_alpha
