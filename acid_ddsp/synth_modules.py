87b07d2d16dc642d2770b1f287fa525091d767ad
christhetree <christhetree@gmail.com>
2024-03-14 20:43:50 +0000

    [cm] Updating ADSR

--- acid_ddsp/synth_modules.py
+++ acid_ddsp/synth_modules.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import random
 from dataclasses import dataclass
 from typing import Optional, Dict
 
@@ -8,8 +7,7 @@ import torch as tr
 from torch import Tensor as T, nn
 
 from torchsynth.config import SynthConfig
-from torchsynth.module import SquareSawVCO, LFO, ADSR
-from torchsynth.signal import Signal
+from torchsynth.module import ADSR
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -26,11 +24,16 @@ class ADSRValues:
 
 
 class CustomADSR(ADSR):
-    def __init__(self,
-                 synthconfig: SynthConfig,
-                 min_adsr_vals: ADSRValues,
-                 max_adsr_vals: ADSRValues,
-                 **kwargs: Dict[str, T]) -> None:
+    def __init__(
+        self,
+        sr: float,
+        n_samples: int,
+        batch_size: int,
+        min_adsr_vals: ADSRValues,
+        max_adsr_vals: ADSRValues,
+        **kwargs: Dict[str, T],
+    ) -> None:
+        assert int(sr) == sr
         for dr in self.default_parameter_ranges:
             if dr.name == "attack":
                 dr.minimum = min_adsr_vals.attack
@@ -47,32 +50,16 @@ class CustomADSR(ADSR):
             if dr.name == "alpha":
                 dr.minimum = min_adsr_vals.alpha
                 dr.maximum = max_adsr_vals.alpha
-        super().__init__(synthconfig, **kwargs)
-
-
-class CustomLFO(LFO):
-    def __init__(self,
-                 synthconfig: SynthConfig,
-                 lfo_type: Optional[str] = None,
-                 min_freq: float = 0.5,
-                 max_freq: float = 12.0,
-                 **kwargs: Dict[str, T]) -> None:
-        for dr in self.default_ranges:
-            if dr.name == "frequency":
-                dr.minimum = min_freq
-                dr.maximum = max_freq
-                dr.curve = 0.25
-        super().__init__(synthconfig, **kwargs)
-        if lfo_type is None:
-            lfo_type = random.choice(self.lfo_types)
-        else:
-            assert lfo_type in self.lfo_types
-        self.lfo_type = lfo_type
-        for curr_lfo_type in self.lfo_types:
-            if curr_lfo_type == self.lfo_type:
-                self.set_parameter(curr_lfo_type, tr.ones(self.batch_size))
-            else:
-                self.set_parameter(curr_lfo_type, tr.zeros(self.batch_size))
+        sc = SynthConfig(
+            batch_size=batch_size,
+            sample_rate=int(sr),
+            buffer_size_seconds=n_samples / sr,
+            control_rate=int(sr),
+            reproducible=False,
+            no_grad=True,
+            debug=False,
+        )
+        super().__init__(sc, **kwargs)
 
 
 class SquareSawVCOLite(nn.Module):
@@ -113,52 +100,9 @@ class SquareSawVCOLite(nn.Module):
         return out_wave
 
 
-class CustomSquareSawVCO(SquareSawVCO):
-    def __init__(
-        self,
-        synthconfig: SynthConfig,
-        device: Optional[tr.device] = None,
-        **kwargs: Dict[str, T],
-    ):
-        super().__init__(synthconfig, device, **kwargs)
-        self.set_parameter("tuning", tr.zeros((self.batch_size,)))
-        self.set_parameter("mod_depth", tr.zeros((self.batch_size,)))
-
-    def oscillator(self, argument: Signal, midi_f0: T, shape: T) -> Signal:
-        partials = self.partials_constant(midi_f0).unsqueeze(1)
-        square = tr.tanh(tr.pi * partials * tr.sin(argument) / 2)
-        return (1 - shape / 2) * square * (1 + shape * tr.cos(argument))
-
-    def output(self, midi_f0: T, shape_mod_signal: Signal, pitch_mod_signal: Optional[Signal] = None) -> Signal:
-        assert midi_f0.shape == (self.batch_size,)
-        assert shape_mod_signal.shape == (self.batch_size, self.buffer_size)
-
-        if pitch_mod_signal is not None and pitch_mod_signal.shape != (
-            self.batch_size,
-            self.buffer_size,
-        ):
-            raise ValueError(
-                "mod_signal has incorrect shape. Expected "
-                f"{tr.Size([self.batch_size, self.buffer_size])}, "
-                f"and received {pitch_mod_signal.shape}. Make sure the mod_signal "
-                "being passed in is at full audio sampling rate."
-            )
-
-        control_as_frequency = self.make_control_as_frequency(midi_f0, pitch_mod_signal)
-
-        if self.synthconfig.debug:
-            assert (control_as_frequency >= 0).all() and (
-                control_as_frequency <= self.nyquist
-            ).all()
-
-        cosine_argument = self.make_argument(control_as_frequency)
-        cosine_argument += self.p("initial_phase").unsqueeze(1)
-        output = self.oscillator(cosine_argument, midi_f0, shape_mod_signal)
-        return output.as_subclass(Signal)
-
-
 if __name__ == "__main__":
     import torchaudio
+
     freq = tr.tensor([220.0, 220.0])
     osc_shape = tr.tensor([1.0, 0.5])
 
