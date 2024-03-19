65fc001081d75988a3aaf336407bb4d75da86af1
christhetree <christhetree@gmail.com>
2024-03-19 20:10:58 +0000

    [cm] Refactoring lightning

--- acid_ddsp/lightning.py
+++ acid_ddsp/lightning.py
@@ -53,13 +53,22 @@ class AcidDDSPLightingModule(pl.LightningModule):
 
         self.synth = AcidSynth(ac, batch_size)
         self.synth_hat = AcidSynth(ac, batch_size)
-        # self.synth_hat = AcidSynthLearnedBiquad(ac, batch_size)
+        # self.synth_hat = AcidSynthLearnedBiquadCoeff(ac, batch_size)
+        # self.synth_hat = AcidSynthLearnedBiquadPoleZero(ac, batch_size)
         # self.synth_hat = AcidSynthLSTM(ac, batch_size, n_hidden=128)
 
     def on_train_start(self) -> None:
         self.global_n = 0
 
     def step(self, batch: Dict[str, T], stage: str) -> Dict[str, T]:
+        batch_size = self.batch_size
+        if stage == "train":
+            self.global_n = (
+                self.global_step * self.trainer.accumulate_grad_batches * batch_size
+            )
+        # TODO(cm): check if this works for DDP
+        self.log(f"global_n", float(self.global_n), sync_dist=True)
+
         f0_hz = batch["f0_hz"]
         note_on_duration = batch["note_on_duration"]
         mod_sig = batch["mod_sig"]
@@ -77,113 +86,101 @@ class AcidDDSPLightingModule(pl.LightningModule):
             + self.ac.min_osc_shape
         )
 
-        batch_size = self.batch_size
         assert f0_hz.shape == (batch_size,)
         assert osc_shape.shape == (batch_size,)
         assert note_on_duration.shape == (batch_size,)
         assert mod_sig.shape == (batch_size, self.ac.n_samples)
         assert q_norm.shape == (batch_size,)
         assert dist_gain_norm.shape == (batch_size,)
-        if stage == "train":
-            self.global_n = (
-                self.global_step * self.trainer.accumulate_grad_batches * batch_size
-            )
-        # TODO(cm): check if this works for DDP
-        self.log(f"global_n", float(self.global_n), sync_dist=True)
 
         # Generate ground truth audio x
         with tr.no_grad():
             q_mod_sig = tr.ones_like(mod_sig) * q_norm.unsqueeze(-1)
+            filter_args = {
+                "w_mod_sig": mod_sig,
+                "q_mod_sig": q_mod_sig,
+            }
             dry, wet, envelope = self.synth(
-                f0_hz, osc_shape, note_on_duration, mod_sig, q_mod_sig, dist_gain
+                f0_hz, osc_shape, note_on_duration, filter_args, dist_gain
             )
             assert dry.shape == wet.shape == (self.batch_size, self.ac.n_samples)
 
-        # Extract mod_sig_hat
+        # Perform model forward pass
         model_in = wet.unsqueeze(1)
         model_out = self.model(model_in)
-        mod_sig_hat = model_out["mod_sig_hat"]
-        q_norm_hat = model_out["q_norm_hat"]
-        dist_gain_norm_hat = model_out["dist_gain_norm_hat"]
-        osc_shape_norm_hat = model_out["osc_shape_norm_hat"]
-        log_spec = model_out["log_spec"]
-        # a_coeff_hat = model_out.get("a_coeff_hat", None)
-        # b_coeff_hat = model_out.get("b_coeff_hat", None)
+        filter_args_hat = {}
 
-        mod_sig_hat = mod_sig_hat.squeeze(1)
-        q_hat = q_norm_hat * (self.ac.max_q - self.ac.min_q) + self.ac.min_q
+        # Postprocess mod_sig_hat
+        mod_sig_hat = model_out.get("mod_sig_hat", None)
+        if mod_sig_hat is not None:
+            if mod_sig_hat.shape != mod_sig.shape:
+                assert mod_sig_hat.ndim == mod_sig.ndim
+                assert mod_sig_hat.size(-1) < mod_sig.size(-1)
+                mod_sig_hat = util.linear_interpolate_last_dim(
+                    mod_sig_hat, mod_sig.size(-1), align_corners=True
+                )
+            filter_args_hat["w_mod_sig"] = mod_sig_hat
+            with tr.no_grad():
+                mod_sig_esr = self.esr(mod_sig_hat, mod_sig)
+                mod_sig_l1 = self.l1(mod_sig_hat, mod_sig)
+            self.log(f"{stage}/ms_esr", mod_sig_esr, prog_bar=False, sync_dist=True)
+            self.log(f"{stage}/ms_l1", mod_sig_l1, prog_bar=True, sync_dist=True)
+
+        # Postprocess q_hat
+        q_norm_hat = model_out.get("q_norm_hat", None)
+        q_hat = None
+        if q_norm_hat is not None:
+            if self.is_q_learnable:
+                with tr.no_grad():
+                    q_norm_l1 = self.l1(q_norm_hat, q_norm)
+                self.log(f"{stage}/q_l1", q_norm_l1, prog_bar=False, sync_dist=True)
+            q_mod_sig_hat = tr.ones_like(mod_sig) * q_norm_hat.unsqueeze(-1)
+            filter_args_hat["q_mod_sig"] = q_mod_sig_hat
+            q_hat = q_norm_hat * (self.ac.max_q - self.ac.min_q) + self.ac.min_q
+
+        # Postprocess dist_gain_hat
+        dist_gain_norm_hat = model_out["dist_gain_norm_hat"]
         dist_gain_hat = (
             dist_gain_norm_hat * (self.ac.max_dist_gain - self.ac.min_dist_gain)
             + self.ac.min_dist_gain
         )
+        if self.is_dist_gain_learnable:
+            with tr.no_grad():
+                dist_gain_norm_l1 = self.l1(dist_gain_norm_hat, dist_gain_norm)
+            self.log(
+                f"{stage}/dg_l1", dist_gain_norm_l1, prog_bar=False, sync_dist=True
+            )
+
+        # Postprocess osc_shape_hat
+        osc_shape_norm_hat = model_out["osc_shape_norm_hat"]
         osc_shape_hat = (
             osc_shape_norm_hat * (self.ac.max_osc_shape - self.ac.min_osc_shape)
             + self.ac.min_osc_shape
         )
+        if self.is_osc_shape_learnable:
+            with tr.no_grad():
+                osc_shape_norm_l1 = self.l1(osc_shape_norm_hat, osc_shape_norm)
+            self.log(
+                f"{stage}/os_l1", osc_shape_norm_l1, prog_bar=False, sync_dist=True
+            )
 
-        log_spec_audio = None
+        # Postprocess log_spec
+        log_spec = model_out.get("log_spec", None)
         if log_spec is None:
             log_spec = self.spectral_visualizer(model_in)
         assert log_spec.ndim == 4
         log_spec_x = log_spec[:, 0, :, :]
-        if log_spec.size(1) == 2:
-            log_spec_audio = log_spec[:, 1, :, :]
 
-        # import torchaudio
-        # from matplotlib import pyplot as plt
-        # for idx in range(x.size(0)):
-        #     # torchaudio.save(
-        #     #     f"../out/audio_{idx}.wav", osc_audio[idx].unsqueeze(0), self.ac.sr
-        #     # )
-        #     torchaudio.save(f"../out/x_{idx}.wav", x[idx].unsqueeze(0), self.ac.sr)
-        #     # plt.plot(envelope[idx].numpy())
-        #     plt.plot(mod_sig[idx].numpy())
-        #     plt.ylim(0, 1)
-        #     plt.show()
-        #     # plt.imshow(log_spec_dry[idx].numpy(), aspect="auto", origin="lower", cmap="magma")
-        #     # plt.title("Dry")
-        #     # plt.show()
-        #     plt.imshow(
-        #         log_spec_wet[idx, :, :].numpy(), aspect="auto", origin="lower", cmap="magma"
-        #     )
-        #     plt.title("Wet")
-        #     plt.show()
-        # exit()
-
-        if mod_sig_hat.shape != mod_sig.shape:
-            assert mod_sig_hat.ndim == mod_sig.ndim
-            assert mod_sig_hat.size(-1) < mod_sig.size(-1)
-            mod_sig_hat = util.linear_interpolate_last_dim(
-                mod_sig_hat, mod_sig.size(-1), align_corners=True
-            )
-            # a_coeff_hat = tr.swapaxes(a_coeff_hat, 1, 2)
-            # a_coeff_hat = util.linear_interpolate_last_dim(
-            #     a_coeff_hat, mod_sig.size(-1), align_corners=True
-            # )
-            # a_coeff_hat = tr.swapaxes(a_coeff_hat, 1, 2)
-            # b_coeff_hat = tr.swapaxes(b_coeff_hat, 1, 2)
-            # b_coeff_hat = util.linear_interpolate_last_dim(
-            #     b_coeff_hat, mod_sig.size(-1), align_corners=True
-            # )
-            # b_coeff_hat = tr.swapaxes(b_coeff_hat, 1, 2)
-
-        with tr.no_grad():
-            mod_sig_esr = self.esr(mod_sig_hat, mod_sig)
-            mod_sig_l1 = self.l1(mod_sig_hat, mod_sig)
-            q_norm_l1 = 0.0
-            if self.is_q_learnable:
-                q_norm_l1 = self.l1(q_norm_hat, q_norm)
-            dist_gain_norm_l1 = 0.0
-            if self.is_dist_gain_learnable:
-                dist_gain_norm_l1 = self.l1(dist_gain_norm_hat, dist_gain_norm)
-            osc_shape_norm_l1 = 0.0
-            if self.is_osc_shape_learnable:
-                osc_shape_norm_l1 = self.l1(osc_shape_norm_hat, osc_shape_norm)
+        # Postprocess logits
+        logits = model_out.get("logits", None)
+        if logits is not None:
+            assert False  # TODO(cm): interpolate logits
 
         wet_hat = None
         if self.use_p_loss:
+            assert mod_sig_hat is not None
             loss = self.loss_func(mod_sig_hat, mod_sig)
-            if self.is_q_learnable:
+            if self.is_q_learnable and q_norm_hat is not None:
                 q_loss = self.loss_func(q_norm_hat, q_norm)
                 self.log(
                     f"{stage}/ploss_{self.loss_name}_q",
@@ -214,13 +211,11 @@ class AcidDDSPLightingModule(pl.LightningModule):
                 f"{stage}/ploss_{self.loss_name}", loss, prog_bar=True, sync_dist=True
             )
         else:
-            q_mod_sig_hat = tr.ones_like(mod_sig_hat) * q_norm_hat.unsqueeze(-1)
             _, wet_hat, _ = self.synth_hat(
                 f0_hz,
                 osc_shape_hat,
                 note_on_duration,
-                mod_sig_hat,
-                q_mod_sig_hat,
+                filter_args_hat,
                 dist_gain_hat,
             )
             loss = self.loss_func(wet_hat.unsqueeze(1), wet.unsqueeze(1))
@@ -228,11 +223,6 @@ class AcidDDSPLightingModule(pl.LightningModule):
                 f"{stage}/audio_{self.loss_name}", loss, prog_bar=True, sync_dist=True
             )
 
-        self.log(f"{stage}/ms_esr", mod_sig_esr, prog_bar=False, sync_dist=True)
-        self.log(f"{stage}/ms_l1", mod_sig_l1, prog_bar=True, sync_dist=True)
-        self.log(f"{stage}/q_l1", q_norm_l1, prog_bar=False, sync_dist=True)
-        self.log(f"{stage}/dg_l1", dist_gain_norm_l1, prog_bar=False, sync_dist=True)
-        self.log(f"{stage}/os_l1", osc_shape_norm_l1, prog_bar=False, sync_dist=True)
         self.log(f"{stage}/loss", loss, prog_bar=False, sync_dist=True)
 
         out_dict = {
@@ -243,7 +233,6 @@ class AcidDDSPLightingModule(pl.LightningModule):
             "x_hat": wet_hat,
             "osc_audio": dry,
             "envelope": envelope,
-            "log_spec_audio": log_spec_audio,
             "log_spec_x": log_spec_x,
             "q": q,
             "q_hat": q_hat,
--- acid_ddsp/models.py
+++ acid_ddsp/models.py
@@ -33,11 +33,8 @@ class Spectral2DCNN(nn.Module):
         bin_dilations: Optional[List[int]] = None,
         temp_dilations: Optional[List[int]] = None,
         pool_size: Tuple[int, int] = (2, 1),
-        latent_dim: int = 1,
         use_ln: bool = True,
         dropout: float = 0.25,
-        # degree: int = 3,
-        # n_segments: int = 4,
     ) -> None:
         super().__init__()
         self.fe = fe
@@ -45,10 +42,7 @@ class Spectral2DCNN(nn.Module):
         self.kernel_size = kernel_size
         assert pool_size[1] == 1
         self.pool_size = pool_size
-        self.latent_dim = latent_dim
         self.use_ln = use_ln
-        # self.degree = degree
-        # self.n_segments = n_segments
 
         if out_channels is None:
             out_channels = [64] * 5
@@ -86,9 +80,10 @@ class Spectral2DCNN(nn.Module):
             layers.append(nn.PReLU(num_parameters=out_ch))
             in_ch = out_ch
             curr_n_bins = curr_n_bins // pool_size[0]
+
         self.cnn = nn.Sequential(*layers)
 
-        self.out_ms = nn.Conv1d(out_channels[-1], self.latent_dim, kernel_size=(1,))
+        self.out_ms = nn.Linear(out_channels[-1], 1)
 
         self.out_q = nn.Sequential(
             nn.Linear(out_channels[-1], out_channels[-1] // 2),
@@ -114,86 +109,23 @@ class Spectral2DCNN(nn.Module):
             nn.Sigmoid(),
         )
 
-        # self.out_coeff = nn.Sequential(
-        #     nn.Linear(out_channels[-1], out_channels[-1] // 2),
-        #     nn.Dropout(p=dropout),
-        #     nn.PReLU(),
-        #     nn.Linear(out_channels[-1] // 2, 5),
-        # )
-
-        # support = tr.linspace(0.0, 1.0, fe.n_frames).view(1, -1, 1, 1)
-        # support = support.repeat(1, 1, n_segments, degree)
-        #
-        # segment_offsets = tr.linspace(0.0, 1.0, n_segments + 1)[:-1].view(1, 1, -1, 1)
-        # support = support - segment_offsets
-        # support = tr.clamp(support, min=0.0)
-        #
-        # exponent = tr.arange(start=1, end=degree + 1).int().view(1, 1, 1, -1)
-        # # self.register_buffer("exponent", exponent)
-        # support = support.pow(exponent)
-        #
-        # self.register_buffer("support", support)
-        #
-        # fc_out_dim = (out_channels[-1] + degree) // 2
-        # self.fc = nn.Linear(out_channels[-1], fc_out_dim)
-        # self.fc_prelu = nn.PReLU()
-        # self.out_poly = nn.Linear(fc_out_dim, degree)
-        #
-        # self.fc_global = nn.Linear(out_channels[-1], fc_out_dim)
-        # self.fc_global_prelu = nn.PReLU(num_parameters=fc_out_dim)
-        # self.out_global = nn.Linear(fc_out_dim, 1)
-
-        # self.lstm = nn.LSTM(out_channels[-1], out_channels[-1], batch_first=True)
-        # self.lstm = nn.LSTM(fe.n_bins, out_channels[-1], batch_first=True)
-
     def forward(self, x: T) -> Dict[str, T]:
         assert x.ndim == 3
         log_spec = self.fe(x)
 
         x = self.cnn(log_spec)
         x = tr.mean(x, dim=-2)
-        latent = x
+        latent = x.swapaxes(1, 2)
 
-        # x = tr.swapaxes(x, 1, 2)
-        # x, _ = self.lstm(x)
-        # x = tr.swapaxes(x, 1, 2)
-
-        # x = tr.mean(latent, dim=-1)
-        # x = self.fc_global(x)
-        # x = self.fc_global_prelu(x)
-        # x = self.out_global(x)
-        # bias = x
-        # # bias = magic_clamp(x, min_value=0.0, max_value=1.0)
-
-        # x = latent.view(latent.size(0), latent.size(1), self.n_segments, -1)
-        # x = tr.mean(x, dim=-1)
-        # x = x.permute(0, 2, 1)
-        # x = self.fc(x)
-        # x = self.fc_prelu(x)
-        # x = self.out_poly(x)
-        # coeffs = x.unsqueeze(1)
-
-        # support = self.support
-        # x = coeffs * support
-        # x = tr.sum(x, dim=-1)
-        # x = tr.sum(x, dim=-1)
-        # x += bias
-        # x = tr.sigmoid(x)
-        # # x = magic_clamp(x, min_value=0.0, max_value=1.0)
-
-        x = self.out_ms(x)
+        x = self.out_ms(latent)
         ms_hat = tr.sigmoid(x)
         # ms_hat = magic_clamp(x, min_value=0.0, max_value=1.0)
+        ms_hat = ms_hat.squeeze(-1)
 
-        x = tr.mean(latent, dim=-1)
+        x = tr.mean(latent, dim=-2)
         q_norm_hat = self.out_q(x).squeeze(-1)
         dist_gain_norm_hat = self.out_dist_gain(x).squeeze(-1)
         osc_shape_norm_hat = self.out_osc_shape(x).squeeze(-1)
-
-        # x = tr.swapaxes(latent, 1, 2)
-        # coeff = self.out_coeff(x)
-        # a_coeff_hat = coeff[..., :2]
-        # b_coeff_hat = coeff[..., 2:]
 
         return {
             "mod_sig_hat": ms_hat,
@@ -202,8 +134,6 @@ class Spectral2DCNN(nn.Module):
             "osc_shape_norm_hat": osc_shape_norm_hat,
             "latent": latent,
             "log_spec": log_spec,
-            # "a_coeff_hat": a_coeff_hat,
-            # "b_coeff_hat": b_coeff_hat,
         }
 
 
