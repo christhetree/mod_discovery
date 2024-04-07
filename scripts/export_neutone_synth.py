c5ffc13877a4886c3d6f55731ac6aa78257f520b
christhetree <christhetree@gmail.com>
2024-04-07 23:43:51 +0100

    [cm] Better env implementation

--- scripts/export_neutone_synth.py
+++ scripts/export_neutone_synth.py
@@ -25,8 +25,7 @@ class AcidSynth(nn.Module):
         synth: AcidSynthLPBiquad,
         min_midi_f0: int = 30,
         max_midi_f0: int = 60,
-        min_note_on_duration: float = 0.125,
-        max_note_on_duration: float = 0.5,
+        note_on_duration: float = 0.125,
         osc_shape: float = 1.0,
         osc_gain: float = 0.5,
         dist_gain: float = 1.0,
@@ -37,15 +36,14 @@ class AcidSynth(nn.Module):
             synth.toggle_scriptable(True)
         self.min_midi_f0 = min_midi_f0
         self.max_midi_f0 = max_midi_f0
-        self.min_note_on_duration = min_note_on_duration
-        self.max_note_on_duration = max_note_on_duration
-        self.register_buffer("note_on_duration", tr.full((1,), min_note_on_duration))
+        self.register_buffer("note_on_duration", tr.full((1,), note_on_duration))
         self.register_buffer("osc_shape", tr.full((1,), osc_shape))
         self.register_buffer("osc_gain", tr.full((1,), osc_gain))
         self.register_buffer("dist_gain", tr.full((1,), dist_gain))
-        self.register_buffer("alpha", tr.full((1,), 1.0))
+        # self.register_buffer("alpha", tr.full((1,), 1.0))
 
         self.sr = self.synth.ac.sr
+        self.note_on_samples = int(note_on_duration * self.sr)
         self.curr_env_val = 1.0
         self.register_buffer("phase", tr.zeros((1, 1)))
         self.midi_f0_to_hz = {
@@ -58,109 +56,87 @@ class AcidSynth(nn.Module):
         self.phase.zero_()
 
     @staticmethod
-    def make_envelope(
-        x: T, note_on_samples: int, curr_env_val: float, alpha: T
+    def make_envelope_2(
+        x: T, note_on_samples: int, curr_env_val: float
     ) -> Tuple[T, int, float]:
         n_samples = x.size(-1)
-        env = tr.zeros((1, n_samples))
+        n_env = (2 * n_samples) // note_on_samples
+        n_env = max(n_env, 2)
+        env = tr.linspace(1.0, 0.0, note_on_samples)
+        env = env.repeat(n_env)
 
-        # Find the indices of non-zero audio sections
-        audio_idx_pairs: List[Tuple[int, int]] = []
-        prev_curr_silent = True
-        start_idx = -1
-        for idx, val in enumerate(x.squeeze()):
-            if val != 0.0:
-                curr_silent = False
-            else:
-                curr_silent = True
+        out = tr.zeros((1, n_samples))
 
-            if prev_curr_silent and not curr_silent:
-                start_idx = idx
-            elif not prev_curr_silent and curr_silent:
-                audio_idx_pairs.append((start_idx, idx))
-            prev_curr_silent = curr_silent
-        audio_idx_pairs.append((start_idx, n_samples))
-        assert len(audio_idx_pairs) > 0
-        assert all([s < e for s, e in audio_idx_pairs])
+        # Find first non-zero index
+        non_zero_indices = tr.nonzero(x.squeeze())
+        first_nonzero_idx = -1
+        if non_zero_indices.size(0) > 0:
+            first_nonzero_idx = non_zero_indices[0, 0].item()
 
-        first_start_idx = audio_idx_pairs[0][0]
-        if first_start_idx > 0 or curr_env_val == 1.0:
-            curr_env_val = 1.0
-            first_env_n_samples = note_on_samples
-        else:
-            first_env_n_samples = int((note_on_samples - 1) * curr_env_val)
-        first_env_n_samples = max(first_env_n_samples, 2)
+        # If all silence and no envelope is being continued, return silence
+        if first_nonzero_idx == -1 and curr_env_val == 0.0:
+            return out, 0, curr_env_val
 
-        # Split the audio sections into envelopes if they are too long
-        env_idx_pairs: List[Tuple[int, int]] = []
-        max_n_samples = first_env_n_samples
-        for start_idx, end_idx in audio_idx_pairs:
-            if end_idx - start_idx > max_n_samples:
-                curr_start_idx = start_idx
-                while curr_start_idx < end_idx:
-                    curr_end_idx = min(curr_start_idx + max_n_samples, end_idx)
-                    env_idx_pairs.append((curr_start_idx, curr_end_idx))
-                    curr_start_idx = curr_end_idx
-                    max_n_samples = note_on_samples
-            else:
-                env_idx_pairs.append((start_idx, end_idx))
-        assert all([s < e for s, e in env_idx_pairs])
-        if len(env_idx_pairs) >= 3:
-            filtered_env_idx_pairs = [env_idx_pairs[0]]
-            for idx in range(1, len(env_idx_pairs) - 1):
-                start_idx, end_idx = env_idx_pairs[idx]
-                if end_idx - start_idx >= 16:
-                    filtered_env_idx_pairs.append((start_idx, end_idx))
-            filtered_env_idx_pairs.append(env_idx_pairs[-1])
-            env_idx_pairs = filtered_env_idx_pairs
+        # Calc envelope continuation index
+        cont_env_start_idx = 0
+        if curr_env_val != 0.0:
+            cont_env_start_idx = int(
+                round((1.0 - curr_env_val) * (note_on_samples - 1))
+            )
+            # cont_env_start_idx = int(round(cont_env_start_idx))
+            cont_env_start_idx = min(cont_env_start_idx, note_on_samples - 1)
+            cont_env_start_idx = max(cont_env_start_idx, 0)
 
-        env_start_vals = [curr_env_val] + [1.0 for _ in range(len(env_idx_pairs) - 1)]
+        # If there is no silence, return the envelope
+        if first_nonzero_idx == 0:
+            out[:, 0:n_samples] = env[
+                cont_env_start_idx : cont_env_start_idx + n_samples
+            ]
+            curr_env_val = out[0, -1].item()
+            return out, first_nonzero_idx, curr_env_val
 
-        last_env_start_idx, last_env_end_idx = env_idx_pairs[-1]
-        last_env_n_samples = last_env_end_idx - last_env_start_idx
-        slope = 1.0 / (note_on_samples - 1)
-        last_env_start_val = env_start_vals[-1]
-        last_env_end_val = last_env_start_val - (last_env_n_samples * slope)
-        last_env_end_val = max(last_env_end_val, 0.0)
-        env_end_vals = [0.0 for _ in range(len(env_idx_pairs) - 1)] + [last_env_end_val]
+        # Continue the envelope if required
+        cont_env_len = 0
+        if curr_env_val != 0.0:
+            cont_env_len = min(n_samples, note_on_samples - cont_env_start_idx)
+            cont_env_end_idx = cont_env_start_idx + cont_env_len
+            out[:, 0:cont_env_len] = env[cont_env_start_idx:cont_env_end_idx]
 
-        for idx in range(len(env_idx_pairs)):
-            start_idx, end_idx = env_idx_pairs[idx]
-            start_val = env_start_vals[idx]
-            end_val = env_end_vals[idx]
-            env_n_samples = end_idx - start_idx
-            if start_val == 1.0:
-                env_section = tr.linspace(start_val, end_val, env_n_samples)
-            else:
-                env_section = tr.linspace(start_val, end_val, env_n_samples + 1)[1:]
-            env[:, start_idx:end_idx] = env_section
+        # If all silence
+        if first_nonzero_idx == -1:
+            curr_env_val = out[0, -1].item()
+            return out, 0, curr_env_val
 
-        return env, first_start_idx, last_env_end_val
+        first_nonzero_idx = max(first_nonzero_idx, cont_env_len)
+        n_non_zero_samples = n_samples - first_nonzero_idx
+        out[:, first_nonzero_idx:n_samples] = env[0:n_non_zero_samples]
+        curr_env_val = out[0, -1].item()
+        return out, first_nonzero_idx, curr_env_val
 
     def forward(
         self,
         x: T,
         midi_f0_0to1: T,
-        note_on_duration_0to1: T,
+        # note_on_duration_0to1: T,
+        alpha_0to1: T,
         w_mod_sig: T,
         q_mod_sig: T,
     ) -> T:
-        if x.min() == x.max() == 0.0:
-            self.curr_env_val = 1.0
-            self.phase.zero_()
-            return x
-
         n_samples = x.size(-1)
-        note_on_duration = (
-            note_on_duration_0to1
-            * (self.max_note_on_duration - self.min_note_on_duration)
-            + self.min_note_on_duration
-        )
-        note_on_samples = int(note_on_duration.item() * self.sr)
-        env, env_start_idx, new_env_val = self.make_envelope(
-            x, note_on_samples, self.curr_env_val, self.alpha
+        # note_on_duration = (
+        #     note_on_duration_0to1
+        #     * (self.max_note_on_duration - self.min_note_on_duration)
+        #     + self.min_note_on_duration
+        # )
+        # note_on_samples = int(note_on_duration.item() * self.sr)
+        alpha = self.synth.ac.convert_from_0to1("learned_alpha", alpha_0to1)
+
+        env, env_start_idx, new_env_val = self.make_envelope_2(
+            x, self.note_on_samples, self.curr_env_val
         )
         self.curr_env_val = new_env_val
+        if alpha != 1.0:
+            tr.pow(env, alpha, out=env)
 
         midi_f0 = (
             midi_f0_0to1 * (self.max_midi_f0 - self.min_midi_f0) + self.min_midi_f0
@@ -168,7 +144,6 @@ class AcidSynth(nn.Module):
         midi_f0 = midi_f0.round().int().item()
         f0_hz = self.midi_f0_to_hz[midi_f0]
 
-        # alpha = self.synth.ac.convert_from_0to1("learned_alpha", alpha_0to1)
 
         filter_args = {
             "w_mod_sig": w_mod_sig,
@@ -178,7 +153,7 @@ class AcidSynth(nn.Module):
             "osc_shape": self.osc_shape,
             "osc_gain": self.osc_gain,
             "dist_gain": self.dist_gain,
-            "learned_alpha": self.alpha,
+            "learned_alpha": alpha,
         }
         synth_out = self.synth(
             n_samples=n_samples,
@@ -190,11 +165,10 @@ class AcidSynth(nn.Module):
             envelope=env,
         )
         wet = synth_out["wet"]
-        dry = synth_out["dry"]
 
         period_completion = (n_samples / (self.sr / f0_hz.item())) % 1.0
         tr.add(self.phase, 2 * tr.pi * period_completion, out=self.phase)
-        return dry
+        return wet
 
 
 class AcidSynthWrapper(WaveformToWaveformBase):
@@ -231,8 +205,8 @@ class AcidSynthWrapper(WaveformToWaveformBase):
     def get_neutone_parameters(self) -> List[NeutoneParameter]:
         return [
             NeutoneParameter("midi_f0", "midi_f0", default_value=0.5),
-            NeutoneParameter("note_on_duration", "note_on_duration", default_value=0.0),
-            # NeutoneParameter("alpha", "alpha", default_value=0.5),
+            # NeutoneParameter("note_on_duration", "note_on_duration", default_value=0.0),
+            NeutoneParameter("alpha", "alpha", default_value=0.25),
             NeutoneParameter("w_mod_sig", "w_mod_sig", default_value=1.0),
             NeutoneParameter("q_mod_sig", "q_mod_sig", default_value=0.5),
         ]
@@ -254,7 +228,6 @@ class AcidSynthWrapper(WaveformToWaveformBase):
 
     @tr.jit.export
     def get_native_buffer_sizes(self) -> List[int]:
-        # return [6000]
         return []
 
     @tr.jit.export
@@ -265,29 +238,33 @@ class AcidSynthWrapper(WaveformToWaveformBase):
     def do_forward_pass(self, x: T, params: Dict[str, T]) -> T:
         midi_f0_0to1 = params["midi_f0"]
         midi_f0_0to1 = midi_f0_0to1.mean().unsqueeze(0)
-        note_on_duration_0to1 = params["note_on_duration"]
-        note_on_duration_0to1 = note_on_duration_0to1.mean().unsqueeze(0)
+        # note_on_duration_0to1 = params["note_on_duration"]
+        # note_on_duration_0to1 = note_on_duration_0to1.mean().unsqueeze(0)
         w_mod_sig = params["w_mod_sig"].unsqueeze(0)
         q_mod_sig = params["q_mod_sig"].unsqueeze(0)
-        # alpha_0to1 = params["alpha"]
-        # alpha_0to1 = alpha_0to1.mean().unsqueeze(0)
+        alpha_0to1 = params["alpha"]
+        alpha_0to1 = alpha_0to1.mean().unsqueeze(0)
         x = x.unsqueeze(1)
-        y = self.model(x, midi_f0_0to1, note_on_duration_0to1, w_mod_sig, q_mod_sig)
+        # y = self.model(x, midi_f0_0to1, note_on_duration_0to1, w_mod_sig, q_mod_sig)
+        y = self.model(x, midi_f0_0to1, alpha_0to1, w_mod_sig, q_mod_sig)
         y = y.squeeze(1)
         return y
 
 
 if __name__ == "__main__":
-    # buffer_n_samples = 20
-    # curr_env_val = 1.0
+    # buffer_n_samples = 200
+    # curr_env_val = 0.0
     # alpha = 1.0
-    # # note_on_samples_all = [30] * 5
-    # note_on_samples_all = [5, 5]
+    # note_on_samples_all = [300] * 5
     # envs = []
     # for idx, note_on_samples in enumerate(note_on_samples_all):
-    #     x = tr.randn((buffer_n_samples,))
     #     if idx == 0:
-    #         x[0:4] = 0.0
+    #         x = tr.randn((buffer_n_samples,))
+    #         x[0:10] = 0.0
+    #     elif idx == 1:
+    #         x = tr.zeros((buffer_n_samples,))
+    #     else:
+    #         x = tr.randn((buffer_n_samples,))
     #     # x[6:9] = 0.0
     #
     #     env, start_idx, curr_env_val = AcidSynth.make_envelope_2(
@@ -305,8 +282,8 @@ if __name__ == "__main__":
     # exit()
 
     model_dir = MODELS_DIR
-    # model_name = "cnn_mss_lp_td__abstract_303_48k__6k__4k_min__epoch_183_step_1104"
-    model_name = "cnn_mss_lp_fs_128__abstract_303_48k__6k__4k_min__epoch_183_step_1104"
+    model_name = "cnn_mss_lp_td__abstract_303_48k__6k__4k_min__epoch_183_step_1104"
+    # model_name = "cnn_mss_lp_fs_128__abstract_303_48k__6k__4k_min__epoch_183_step_1104"
 
     config_path = os.path.join(model_dir, model_name, "config.yaml")
     ckpt_path = os.path.join(model_dir, model_name, "checkpoints", f"{model_name}.ckpt")
