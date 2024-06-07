80fe02a2771bd2c51e5a692ac1a8d1e7f13f3108
christhetree <christhetree@gmail.com>
2024-06-07 11:07:55 +0100

    [cm] Initial parallel forward pass

--- torchlpc_cpp_ext/experiments.py
+++ torchlpc_cpp_ext/experiments.py
@@ -13,6 +13,10 @@ log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 
 
 if __name__ == "__main__":
+    log.info(f"torch number of threads: {torch.get_num_threads()}")
+    os.environ["OMP_NUM_THREADS"] = f"{torch.get_num_threads()}"
+    # torch.set_num_threads(1)
+
     torch.utils.cpp_extension.load(
         name="forward",
         sources=["torchlpc.cpp"],
@@ -22,23 +26,31 @@ if __name__ == "__main__":
 
     # torch.ops.load_library("build/torchlpc.so")
 
-    T = 4000
-    x = torch.randn(2, T, dtype=torch.double)
-    a = torch.randn(2, T, 3, dtype=torch.double)
-    zi = torch.randn(2, 3, dtype=torch.double)
+    # T = 10
+    # T = 2000
+    T = 100000
+    # T = 200000
+    bs = 2
+    order = 3
 
+    x = torch.randn(bs, T, dtype=torch.double)
+    a = torch.randn(bs, T, order, dtype=torch.double)
+    zi = torch.randn(bs, order, dtype=torch.double)
+
+    log.info("Testing")
     start = time.time()
     og = sample_wise_lpc(x, a, zi)
     end = time.time()
     log.info(f"Testing sample_wise_lpc: {end - start:.4f}s")
-    start = time.time()
-    scriptable = sample_wise_lpc_scriptable(x, a, zi)
-    end = time.time()
-    log.info(f"Testing sample_wise_lpc_scriptable: {end - start:.4f}s")
+    # start = time.time()
+    # scriptable = sample_wise_lpc_scriptable(x, a, zi)
+    # end = time.time()
+    # log.info(f"Testing sample_wise_lpc_scriptable: {end - start:.4f}s")
     start = time.time()
     cpp = torch.ops.torchlpc.forward(x, a, zi)
     end = time.time()
     log.info(f"Testing sample_wise_lpc_cpp: {end - start:.4f}s")
+
     eps = 1e-8
-    log.info(torch.allclose(og, scriptable, atol=eps))
+    # log.info(torch.allclose(og, scriptable, atol=eps))
     log.info(torch.allclose(og, cpp, atol=eps))
--- torchlpc_cpp_ext/torchlpc.cpp
+++ torchlpc_cpp_ext/torchlpc.cpp
@@ -1,5 +1,6 @@
 #include <torch/extension.h>
 #include <torch/script.h>
+#include "/opt/homebrew/opt/libomp/include/omp.h"
 
 torch::Tensor torchlpc_forward(torch::Tensor x, torch::Tensor a, torch::Tensor zi) {
     // Ensure input dimensions are correct
@@ -18,18 +19,62 @@ torch::Tensor torchlpc_forward(torch::Tensor x, torch::Tensor a, torch::Tensor z
 
     // Flip zi and a to match scipy.signal.lfilter
     zi = torch::flip(zi, {1});
-    a = torch::flip(a, {2});
+    // This is not needed when doing the nested for loop approach
+//    a = torch::flip(a, {2});
 
     // Concatenate zi and x along the time dimension
     auto padded_y = torch::cat({zi, x}, 1);
+//    int result = 0;
 
     // Perform the computation for each time step
-    for (int64_t t = 0; t < T; ++t) {
-        auto a_slice = a.slice(1, t, t + 1);
-        auto y_slice = padded_y.slice(1, t, t + order).unsqueeze(2);
-        auto prod = torch::matmul(a_slice, y_slice).squeeze(2);
-        padded_y.slice(1, t + order, t + order + 1) -= prod;
-    }
+//    omp_set_num_threads(10);
+//    #pragma omp parallel for reduction(-:padded_y)
+//    #pragma omp parallel for reduction(+:result)
+//    for (int64_t t = 0; t < T; ++t) {
+//        auto a_slice = a.slice(1, t, t + 1);
+//        auto y_slice = padded_y.slice(1, t, t + order).unsqueeze(2);
+//        auto prod = torch::matmul(a_slice, y_slice).squeeze(2);
+//        padded_y.slice(1, t + order, t + order + 1) -= prod;
+//    }
+//    at::parallel_for(0, T * 40000, 100, [&](int64_t begin, int64_t end) {
+//        for (auto t = begin; t < end; ++t) {
+////            result += 1;
+//            auto a_slice = a.slice(1, t, t + 1);
+//            auto y_slice = padded_y.slice(1, t, t + order).unsqueeze(2);
+//            auto prod = torch::matmul(a_slice, y_slice).squeeze(2);
+//            padded_y.slice(1, t + order, t + order + 1) -= prod;
+//        }
+//    });
+//    assert(result != 0);
+
+//    for (int64_t b = 0; b < B; ++b) {
+//        for (int64_t t = 0; t < T; ++t) {
+//            auto ref = padded_y.index({b, t + order});
+//            for (int64_t i = 0; i < order; ++i) {
+//                auto a_val = a.index({b, t, i});
+//                auto y_val = padded_y.index({b, t + order - i - 1});
+//                auto prod = a_val * y_val;
+//                ref -= prod;
+//            }
+//            padded_y.index_put_({b, t + order}, ref);
+//        }
+//    }
+    at::parallel_for(0, B, 1, [&](int64_t begin_b, int64_t end_b) {
+        for (auto b = begin_b; b < end_b; ++b) {
+            for (int64_t t = 0; t < T; ++t) {
+                auto ref = padded_y.index({b, t + order});
+                at::parallel_for(0, order, 1, [&](int64_t begin_i, int64_t end_i) {
+                    for (auto i = begin_i; i < end_i; ++i) {
+                        auto a_val = a.index({b, t, i});
+                        auto y_val = padded_y.index({b, t + order - i - 1});
+                        auto prod = a_val * y_val;
+                        ref -= prod;
+                    }
+                });
+                padded_y.index_put_({b, t + order}, ref);
+            }
+        }
+    });
 
     // Remove the padding and return the result
     auto y = padded_y.slice(1, order, T + order);
